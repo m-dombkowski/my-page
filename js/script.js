bd8321846a818e508814089890a550d75d823ef3
Mateusz Dombkowski <dombkowskimateusz@gmail.com>
2022-01-02 21:47:41 +0100

    Responisve, mobile nav, dark/ligh mode, male tweaki w css

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
+// ------------------- STICKY NAV -----------------------
+
 const navigation = document.querySelector(".navigation");
+
+const headerEL = document.querySelector(".header");
 
 let navigationTop = navigation.offsetTop;
 
+// const stickyNav = function () {
+//   if (window.scrollY > navigationTop) {
+//     navigation.classList.add("sticky");
+//   } else {
+//     navigation.classList.remove("sticky");
+//   }
+// };
+
 const stickyNav = function () {
   if (window.scrollY > navigationTop) {
-    navigation.classList.add("sticky");
+    headerEL.classList.add("sticky");
   } else {
-    navigation.classList.remove("sticky");
+    headerEL.classList.remove("sticky");
   }
 };
 
 window.addEventListener("scroll", stickyNav);
+
+// ---------------- SMOOTH SCROLL --------------------
 
 const allLinks = document.querySelectorAll(".nav-element");
 
@@ -36,3 +50,51 @@ allLinks.forEach(function (link) {
     }
   });
 });
+
+// ------------------- MOBILE NAV ----------------
+
+const mobileNav = document.querySelector(".btn-mobile-nav");
+
+// const headerEL = document.querySelector(".header");
+
+mobileNav.addEventListener("click", function () {
+  headerEL.classList.toggle("nav-open");
+});
+
+const navLinks = document.querySelectorAll(".nav-element");
+
+navLinks.forEach(function (link) {
+  link.addEventListener("click", function () {
+    headerEL.classList.toggle("nav-open");
+  });
+});
+
+const navIcon = document.getElementById("#nav-icon3");
+
+const modeButton = document.querySelector(".btn-light-dark-mode");
+
+// ---------------- DARK/LIGHT MODE ------------------------
+
+modeButton.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+});
+
+const body = document.body;
+const sun = document.querySelector(".sun");
+const moon = document.querySelector(".moon");
+
+const btnMode = document.querySelector(".btn-light-dark-mode");
+
+const mode = function () {
+  if (body.classList.contains("dark")) {
+    sun.style.display = "block";
+    moon.style.display = "none";
+  } else {
+    sun.style.display = "none";
+    moon.style.display = "block";
+  }
+};
+
+btnMode.addEventListener("click", mode);
+
+mode();
